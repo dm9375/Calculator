2f3ce94c93e630c3e910a6c26760fb76ef0f8979
Daniel Mironiuk <dm9375@rit.edu>
2019-09-13 14:06:46 -0400

    Got color class done

--- src/rit/Color.java
+++ src/rit/Color.java
@@ -3,13 +3,16 @@ package rit;
  * This Object holds an RGB color definition.
  * 
  * WHAT PRICIPLE IS VIOLATED?
- * <<your answer here>>
+ * Well technically both the Law of Demeter & the Single Responsibility principles are being
+ * violated based on the premise that coupling and cohesion have an inverse relationship
+ * with each other so that means if these instance variables were creating interdependances
+ * which shouldn't occur.
  */
 public class Color {
-  public int red;
-  public int green;
-  public int blue;
-  
+  private int red;
+  private int green;
+  private int blue;
+
   public String getCssCode() {
     return "#"
         + Integer.toHexString(red)
